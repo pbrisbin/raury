6ce709c7b1f6ca343d256f28e7d907fb9abdb43b
patrick brisbin <pbrisbin@gmail.com>
2012-05-05 13:09:07 -0400

    implement downloads

---
+++ lib/raury/download.rb
@@ -0,0 +1,26 @@
+module Raury
+  class Download
+    def initialize(package)
+      @pkg_url = Info.new([package]).call.first.pkg_url
+      @aur     = Aur.new(@pkg_url)
+    end
+
+    # downloads the taurball in current directory
+    def download
+      target = File.basename(@pkg_url)
+
+      File.open(target, 'w') do |fh|
+        fh.write(@aur.fetch)
+      end
+    end
+
+    # downloads the taurball directly to tar, extracting it in the
+    # current directory
+    def extract
+      IO.popen('tar fxz -', 'w') do |h|
+        h.write(@aur.fetch)
+        h.close_write
+      end
+    end
+  end
+end
---
+++ spec/download_spec.rb
@@ -0,0 +1,32 @@
+require 'raury'
+
+describe Raury::Download do
+  it "should download a taurball" do
+    result = double("result", :pkg_url => '/a/url.tar.gz')
+
+    Raury::Info.any_instance.stub(:call).and_return([result])
+    Raury::Aur.any_instance.stub(:fetch).and_return('some data')
+
+    fh = double("fh")
+    fh.should_receive(:write).with('some data')
+
+    File.should_receive(:open).with('url.tar.gz', 'w').and_yield(fh)
+
+    Raury::Download.new('foo').download
+  end
+
+  it "should extract directly" do
+    result = double("result", :pkg_url => '/a/url')
+
+    Raury::Info.any_instance.stub(:call).and_return([result])
+    Raury::Aur.any_instance.stub(:fetch).and_return('some data')
+
+    h = double("h")
+    h.should_receive(:write).with('some data')
+    h.should_receive(:close_write)
+
+    IO.should_receive(:popen).with('tar fxz -', 'w').and_yield(h)
+
+    Raury::Download.new('foo').extract
+  end
+end
