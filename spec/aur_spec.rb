048cacb61c41514347dbb4d29b1c5054fc2c04b3
patrick brisbin <pbrisbin@gmail.com>
2012-05-20 17:35:48 -0400

    update aur_spec

--- spec/aur_spec.rb
+++ spec/aur_spec.rb
@@ -1,8 +1,15 @@
 require 'raury'
 
 describe Raury::Aur do
-  it "delegates to net/http" do
-    Net::HTTP.stub(:get).and_return("foo")
+  it "delegates to Net::HTTP and uses https" do
+    resp = double("resp")
+    resp.stub(:body).and_return('foo')
+
+    http = double("http")
+    http.should_receive(:use_ssl=).with(true)
+    http.should_receive(:request_get).with("/foo?").and_return(resp)
+
+    Net::HTTP.should_receive(:new).with(Raury::Aur::AUR, 443).and_return(http)
 
     aur = Raury::Aur.new("/foo")
     aur.fetch.should eq("foo")
