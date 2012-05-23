fb8b87f28d4866390b01f6f42f437f6bfc513096
patrick brisbin <pbrisbin@gmail.com>
2012-05-23 12:55:44 -0400

    use smaller variables

--- lib/raury/options.rb
+++ lib/raury/options.rb
@@ -6,45 +6,45 @@ module Raury
 
       def parse!(argv)
         command = nil
-        config  = Config.config
+        conf    = Config.config
 
-        OptionParser.new do |opts|
-          opts.banner =  'usage: raury [command] [options] [arguments]'
-          opts.separator ''
-          opts.separator 'Commands:'
-          opts.on(            '-S', '--sync',           'Process packages')        { command = :install }
-          opts.on(            '-u', '--upgrade',        'Upgrade packages')        { command = :upgrade }
-          opts.on(            '-s', '--search',         'Search for packages')     { command = :search  }
-          opts.on(            '-i', '--info',           'Show info for packages')  { command = :info    }
-          opts.separator ''
-          opts.separator 'Options:'
-          opts.on(            '-d', '--download',       'Stop after downloading')  { config['sync_level'] = :download }
-          opts.on(            '-e', '--extract',        'Stop after extracting')   { config['sync_level'] = :extract  }
-          opts.on(            '-b', '--build',          'Stop after building')     { config['sync_level'] = :build    }
-          opts.on(            '-y', '--install',        'Install after building')  { config['sync_level'] = :install  }
-          opts.separator ''
-          opts.on(                  '--build-dir DIR',  'Set build directory')     { |d| config['build_directory'] = d }
-          opts.on(                  '--ignore PKG',     'Ignore package')          { |p| config['ignores'] << p }
-          opts.on(                  '--[no-]color',     'Colorize output')         { |b| config['color']   = b }
-          opts.on(                  '--[no-]confirm',   'Auto-answer prompts')     { |b| config['confirm'] = b }
-          opts.on(                  '--[no-]deps',      'Resolve dependencies')    { |b| config['resolve'] = b }
-          opts.on(                  '--[no-]edit',      'Edit PKGBUILDs')          { |b| config['edit']    = b ? :always : :never }
-          opts.separator ''
-          opts.on(                  '--version',        'Show version')            { puts "raury #{VERSION}"; exit }
-          opts.on(                  '--debug',          'Show debug output')       { config['debug'] = true }
-          opts.separator ''
-          opts.on(            '-h', '--help',           'Display this screen')     { puts opts; exit }
-          opts.separator ''
-          opts.separator 'These options can be passed to makepkg:'
-          opts.separator ''
-          mopt(opts, config,   '-c', '--clean',         'Clean up work files after build')
-          mopt(opts, config,   '-f', '--force',         'Overwrite existing package')
-          mopt(opts, config,   '-L', '--log',           'Log package build process')
-          mopt(opts, config,   '-r', '--rmdeps',        'Remove installed dependencies after a successful build')
-          mopt(opts, config,         '--asroot',        'Allow makepkg to run as root user')
-          mopt(opts, config,         '--sign',          'Sign the resulting package with gpg')
-          mopt(opts, config,         '--skipinteg',     'Do not perform any verification checks on source files')
-          opts.separator ''
+        OptionParser.new do |o|
+          o.banner =  'usage: raury [command] [options] [arguments]'
+          o.separator ''
+          o.separator 'Commands:'
+          o.on(            '-S', '--sync',           'Process packages')        { command = :install }
+          o.on(            '-u', '--upgrade',        'Upgrade packages')        { command = :upgrade }
+          o.on(            '-s', '--search',         'Search for packages')     { command = :search  }
+          o.on(            '-i', '--info',           'Show info for packages')  { command = :info    }
+          o.separator ''
+          o.separator 'Options:'
+          o.on(            '-d', '--download',       'Stop after downloading')  { conf['sync_level'] = :download }
+          o.on(            '-e', '--extract',        'Stop after extracting')   { conf['sync_level'] = :extract  }
+          o.on(            '-b', '--build',          'Stop after building')     { conf['sync_level'] = :build    }
+          o.on(            '-y', '--install',        'Install after building')  { conf['sync_level'] = :install  }
+          o.separator ''
+          o.on(                  '--build-dir DIR',  'Set build directory')     { |d| conf['build_directory'] = d }
+          o.on(                  '--ignore PKG',     'Ignore package')          { |p| conf['ignores'] << p }
+          o.on(                  '--[no-]color',     'Colorize output')         { |b| conf['color']   = b }
+          o.on(                  '--[no-]confirm',   'Auto-answer prompts')     { |b| conf['confirm'] = b }
+          o.on(                  '--[no-]deps',      'Resolve dependencies')    { |b| conf['resolve'] = b }
+          o.on(                  '--[no-]edit',      'Edit PKGBUILDs')          { |b| conf['edit']    = b ? :always : :never }
+          o.separator ''
+          o.on(                  '--version',        'Show version')            { puts "raury #{VERSION}"; exit }
+          o.on(                  '--debug',          'Show debug output')       { conf['debug'] = true }
+          o.separator ''
+          o.on(            '-h', '--help',           'Display this screen')     { puts o; exit }
+          o.separator ''
+          o.separator 'These options can be passed to makepkg:'
+          o.separator ''
+          mopt(o, conf,    '-c', '--clean',         'Clean up work files after build')
+          mopt(o, conf,    '-f', '--force',         'Overwrite existing package')
+          mopt(o, conf,    '-L', '--log',           'Log package build process')
+          mopt(o, conf,    '-r', '--rmdeps',        'Remove installed dependencies after a successful build')
+          mopt(o, conf,          '--asroot',        'Allow makepkg to run as root user')
+          mopt(o, conf,          '--sign',          'Sign the resulting package with gpg')
+          mopt(o, conf,          '--skipinteg',     'Do not perform any verification checks on source files')
+          o.separator ''
 
         end.parse!(argv)
 
@@ -56,8 +56,8 @@ module Raury
 
       private
 
-      def mopt(opts, config, *args)
-        opts.on(*args) { config['makepkg_options'] << args.first }
+      def mopt(o, conf, *args)
+        o.on(*args) { conf['makepkg_options'] << args.first }
       end
 
     end
