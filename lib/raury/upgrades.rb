d05f3b0abf3ca3f513c00b133051acc8b1fe85e5
patrick brisbin <pbrisbin@gmail.com>
2012-05-22 20:14:27 -0400

    add debug output to upgrades

--- lib/raury/upgrades.rb
+++ lib/raury/upgrades.rb
@@ -1,38 +1,44 @@
 module Raury
   class Upgrades
-    def self.build_plan
-      local_results = `pacman -Qm`.split("\n").map do |line|
-        name, version = line.split(' ')
+    class << self
+      include Output
 
-        Result.new(:multiinfo, {"Name" => name, "Version" => version}) rescue []
-      end
+      def build_plan
+        local_results = `pacman -Qm`.split("\n").map do |line|
+          name, version = line.split(' ')
 
-      results = []
-      threads = []
-
-      if local_results.any?
-        local_results.each do |local_result|
-          threads << Thread.new do
-            result = Rpc.new(:info, local_result.name).call rescue nil
-
-            if result && Vercmp.new(result.version) > Vercmp.new(local_result.version)
-              results << result
-            end
-          end
+          Result.new(:multiinfo, {"Name" => name, "Version" => version}) rescue []
         end
 
-        threads.map(&:join)
+        results = []
+        threads = []
+
+        if local_results.any?
+          local_results.each do |local_result|
+            threads << Thread.new do
+              debug("fetching info for #{local_result}")
+              result = Rpc.new(:info, local_result.name).call rescue nil
+
+              if result && Vercmp.new(result.version) > Vercmp.new(local_result.version)
+                debug("available upgrade #{local_result} => #{result}")
+                results << result
+              end
+            end
+          end
+
+          threads.map(&:join)
+        end
+
+        if results.empty?
+          puts 'there is nothing to do'
+          exit
+        end
+
+        bp = BuildPlan.new
+        bp.set_results(results.sort)
+
+        bp
       end
-
-      if results.empty?
-        puts 'there is nothing to do'
-        exit
-      end
-
-      bp = BuildPlan.new
-      bp.set_results(results.sort)
-
-      bp
     end
   end
 end
