b274df609de6012b4910f41a4c07ad0d571af947
patrick brisbin <pbrisbin@gmail.com>
2012-05-25 17:15:23 -0400

    add a PKGBUILD Parser
    This provides both the accurate but dangerous ::source! as well as the
    safe but fuzzier ::parse!. That way, end users can easily decide which
    to use.

---
+++ lib/raury/parser.rb
@@ -0,0 +1,64 @@
+module Raury
+  # Retrieves dependencies out of a given PKGBUILD.
+  class Parser
+    class << self
+      # feed the pkgbuild through bash. obviously this comes with all
+      # the risks of sourcing an unviewed PKGBUILD. however, it's the
+      # only way to be 100% accurate.
+      def source!(pkgbuild, build_only)
+        IO.popen('bash', 'r+') do |h|
+          h.write(%{
+#{pkgbuild}
+printf "%s\\n" "${makedepends[@]}"#{build_only ? '' : ' "${depends[@]}"'}
+          })
+
+          h.close_write
+          h.read.split("\n")
+        end
+      end
+
+      # use ruby to parse the (make)depends from the PKGBUILD string.
+      # this is fairly accurately given well-formed PKGBUILDs and is
+      # almost (but not quite) as fast as sourcing directly. however,
+      # it's 100% safe to do.
+      def parse!(pkgbuild, build_only = false)
+        have_deps  = build_only
+        have_mdeps = false
+
+        lines = pkgbuild.dup.split("\n")
+
+        [].tap do |arr|
+          while line = lines.shift
+            if line =~ /makedepends=(.*)/
+              parse_bash_array!(arr, $1, lines)
+              have_mdeps = true
+
+              break if have_deps
+            elsif line =~ /(?!make)depends=(.*)/
+              unless build_only
+                parse_bash_array!(arr, $1, lines)
+                have_deps = true
+
+                break if have_mdeps
+              end
+            end
+          end
+        end
+      end
+
+      private
+
+      def parse_bash_array!(arr, line, lines)
+        while !line.include?(')')
+          line += ' ' + lines.shift.sub(/#.*/, '')
+        end
+
+        if line =~ /\((.*)\)/
+          $1.strip.split(' ').map do |elem|
+            arr << (elem =~ /('|")(.*)\1/ ? $2 : elem)
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/parser_spec.rb
@@ -0,0 +1,66 @@
+require 'raury'
+
+describe Raury::Parser do
+  def build_pkgbuild(deps_str, mdeps_str)
+    %{
+name=whatever
+version=whatever
+depends=( #{deps_str} )
+makedepends=( #{mdeps_str} )
+
+build() {
+  true
+}
+    }.strip
+  end
+
+  it "should parse a simple pkgbuild" do
+    pkgbuild = build_pkgbuild('foo bar baz', 'bat biz qui')
+
+    deps = Raury::Parser.parse!(pkgbuild)
+    deps.sort.should eq(['foo', 'bar', 'baz', 'bat', 'biz', 'qui'].sort)
+  end
+
+  it "should handle mixed quoting and spacing" do
+    pkgbuild = build_pkgbuild("foo 'bar'    baz", '  bat "biz" qui')
+
+    deps = Raury::Parser.parse!(pkgbuild)
+    deps.sort.should eq(['foo', 'bar', 'baz', 'bat', 'biz', 'qui'].sort)
+  end
+
+  it "should handle newlines" do
+    pkgbuild = build_pkgbuild("foo \n'bar' baz", "\n  " + 'bat "biz" qui')
+
+    deps = Raury::Parser.parse!(pkgbuild)
+    deps.sort.should eq(['foo', 'bar', 'baz', 'bat', 'biz', 'qui'].sort)
+  end
+
+  it "should handle comments" do
+    pkgbuild = build_pkgbuild("foo \n# a comment\n  'bar' # a comment\nbaz", "\n  " + 'bat "biz" qui')
+
+    deps = Raury::Parser.parse!(pkgbuild)
+    deps.sort.should eq(['foo', 'bar', 'baz', 'bat', 'biz', 'qui'].sort)
+  end
+
+  it "should handle empty/nonexistent" do
+    pkgbuild = %{
+name=whatever
+version=whatever
+makedepends=( )
+
+build() {
+  true
+}
+    }.strip
+
+    deps = Raury::Parser.parse!(pkgbuild)
+    deps.should be_empty
+  end
+
+  it "should respect build_only" do
+    pkgbuild = build_pkgbuild('foo bar baz', 'bat biz qui')
+
+    deps = Raury::Parser.parse!(pkgbuild, true)
+    deps.sort.should eq(['bat', 'biz', 'qui'].sort)
+  end
+end
