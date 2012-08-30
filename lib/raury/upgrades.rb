1b568c71a18189c8644298c14022a7420652d40a
patrick brisbin <pbrisbin@gmail.com>
2012-08-30 08:59:15 -0400

    Speed up upgrade check
    Loop only once, and do it threadedly.

--- lib/raury/upgrades.rb
+++ lib/raury/upgrades.rb
@@ -8,27 +8,21 @@ module Raury
 
       # add available upgrades to the build plan as additional results.
       def add_to(plan)
-        local_results = []
+        debug("checking available versions of foreign packages")
 
-        pacman_Qm.each do |name,version|
+        each_threaded(pacman_Qm) do |name,version|
           if Config.development_pkg?(name)
             debug("ignoring #{name} due to development regex")
-          else
-            local_results << Result.new(:info, {"Name" => name, "Version" => version})
+            next
           end
-        end
 
-        if local_results.any?
-          debug("checking available versions of foreign packages")
+          local_result  = Result.new(:info, {"Name" => name, "Version" => version})
+          remote_result = Rpc.new(:info, name).call rescue nil
 
-          each_threaded(local_results) do |local_result|
-            result = Rpc.new(:info, local_result.name).call rescue nil
-
-            debug("installed: #{local_result}, available: #{result || 'none'}")
-            if result && result.newer?(local_result)
-              debug("adding to build plan: #{local_result} => #{result}")
-              plan.results << result
-            end
+          debug("installed: #{local_result}, available: #{remote_result || 'none'}")
+          if remote_result && remote_result.newer?(local_result)
+            debug("adding to build plan: #{local_result} => #{remote_result}")
+            plan.results << remote_result # TODO: risk of duplicating targets?
           end
         end
       end
